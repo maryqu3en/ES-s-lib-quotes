f1db0089b69c502e45cf5ea3be44c4b66ea448c9
Meriem Soubih <soubih.meriem@gmail.com>
2023-10-05 22:26:42 +0100

    lil update

--- client/app.js
+++ client/app.js
@@ -2,10 +2,12 @@ const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const bookTitle = document.getElementById("book-title");
 const button = document.getElementById("button");
+// const URL = "http://localhost:3030";
+const URL = "https://marys-lib-quotes.onrender.com";
 
 button.addEventListener("click", async () => {
   try {
-    const response = await fetch(`http://localhost:3030/api/quotes/random`);
+    const response = await fetch(`${URL}/api/quotes/random`);
     const data = await response.json();
 
     quote.innerHTML = data.randomQuote.quote;
